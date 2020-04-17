cb8c725e012401f6168ccdf4f92e2a1a927051b9
konopek1 <michal.konopka01@student.wat.edu.pl>
2020-04-17 13:49:37 +0200

    add s3 support remove local files

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,33 @@
 extern crate rouille;
 use std::time::Duration;
 mod scrap_wat;
+mod s3_driver;
 
 static INTERVAL: u64 = 8; // co ile godzin odswizane
-                          // jesli projekt bedzie sie roziwal przebudowa totalna
 
 #[allow(unreachable_code)]
 fn main() {
+  
     let port = std::env::var("PORT").unwrap_or("8888".to_owned());
-
+    
     std::thread::spawn(move || loop {
         scrap_wat::fetch_parse_plan().unwrap();
+  
         std::thread::sleep(Duration::from_secs(3600 * INTERVAL));
     });
 
-    rouille::start_server(String::from("0.0.0.0:") + &port, move |request| {
+    rouille::start_server(String::from("0.0.0.0:") + &port,  move |request| {
         router!(request,
             (GET) (/) => {
                 let group = request.get_param("group");
+                let bucket = s3_driver::get_bucket().expect("Couldnt get bucket from S3");
+                
                 match group {
                     Some(group) => {
                         if !group.starts_with(".") && !group.starts_with("/"){
-                        let plan_json = std::fs::read_to_string("groups/".to_owned()+&group[..]).unwrap();
+                        let group_file_name = scrap_wat::FOLDER_GROUP.to_owned() + &group[..];
+                        let plan_json = bucket.get_object_blocking(&group_file_name).expect(&format!("Coulndt read {} from S3",&group_file_name));
+            
                         return rouille::Response::json(&plan_json).with_additional_header("Access-Control-Allow-Origin","*")
                     }
                 },
@@ -38,6 +44,7 @@ fn main() {
                 if response.is_success() {
                     return response;
                 }
+            
                 rouille::Response::redirect_303("/index.html")
             }
         )
--- src/scrap_wat.rs
+++ src/scrap_wat.rs
@@ -1,39 +1,48 @@
-use reqwest;
-use scraper::{Html, Selector};
 use std::prelude::v1::Vec;
-use tokio::fs::File;
-use tokio::prelude::*;
 use std::result::Result;
 use std::thread;
 
+use reqwest;
+use s3::bucket::Bucket;
+use scraper::{Html, Selector};
+use tokio::fs::File;
+use s3::credentials::Credentials;
+use s3::region::Region;
+use tokio::prelude::*;
+use crate::s3_driver;
+use std::error::Error;
+
 const COOLDOWN: std::time::Duration = std::time::Duration::from_secs(7);
 const URL: &str = "https://s1.wcy.wat.edu.pl/ed1/";
-const FOLDER_GROUP: &str = "groups/";
+pub const FOLDER_GROUP: &str = "groups/";
 const VMAX: usize = 22;
 const HMAX: usize = 49;
 
-
-#[derive(serde::Serialize,Clone)]
+type GenericError = Box<dyn Error>;
+#[derive(serde::Serialize, Clone)]
 pub struct Krotka {
     title: String,
-    class: String
+    class: String,
 }
 
 impl Krotka {
-    fn new(title: String,class: String) -> Krotka {
-        return Krotka { title,class };
+    fn new(title: String, class: String) -> Krotka {
+        return Krotka { title, class };
     }
 }
+
 type Task = tokio::task::JoinHandle<Result<(), std::io::Error>>;
 
 #[tokio::main]
-pub async fn fetch_parse_plan() -> Result<(), reqwest::Error> {
+pub async fn fetch_parse_plan() -> Result<(), GenericError> {
     let client: reqwest::Client = build_client().unwrap();
 
     let sid = get_sid(&client, URL).await?;
+
     println!("sid:{}", sid);
 
-    let user_id = std::env::var("USER").expect("ERROR: UserId global var not set");
+    let user_id = std::env::var("USER").expect("ERROR: User global var not set");
+
     let password = std::env::var("PASSWORD").expect("ERROR: Password global var not set");
 
     login(&client, &sid, user_id, password).await?;
@@ -47,10 +56,11 @@ pub async fn fetch_parse_plan() -> Result<(), reqwest::Error> {
         thread::sleep(COOLDOWN);
 
         let task = tokio::spawn(async move {
-
-            let content = process_request(&sido,&group).await;
+            let content = process_request(&sido, &group).await;
+            let bucket: Bucket = s3_driver::get_bucket().unwrap();
             let file_name = FOLDER_GROUP.to_owned() + &group[..];
-            save_to_file_json(&content,&file_name).await;     
+
+            save_to_s3(&content,&file_name,&bucket).await;
 
             Ok(())
         });
@@ -75,59 +85,76 @@ async fn process_request(sido: &String, group: &String) -> Vec<Krotka> {
     krotkas
 }
 
-async fn save_to_file_json(content:&Vec<Krotka>,file_name:&str) {
-    println!("Saved to file: {}",file_name);
-    let mut file = File::create(file_name).await.expect("ERROR: Couldnt create file.");
-    let content = serde_json::to_string(content).unwrap();
-    file.write_all(content.as_bytes()).await.expect("ERROR: Couldnt save to file.");
+async fn save_to_s3(content: &Vec<Krotka>, file_name: &str, bucket: &Bucket) {
+    let content = serde_json::to_string(content);
+    let content = content.unwrap();
+
+    let (_, code) = bucket.put_object(file_name, content.as_bytes(), "text/plain")
+        .await
+        .expect("Saving to s3 failed");
+
+    if code == 200 { println!("Plan saved to S3::{}::{}", bucket.name, file_name); }
 }
 
 //w 4 kolumnie text znajduje się sala
 async fn extract_krotkas(html: String) -> Vec<Krotka> {
     let mut result: Vec<Krotka> = Vec::new();
+
     let selector = Selector::parse(r#"td[class="tdFormList1DSheTeaGrpHTM3"]"#).unwrap();
+
     let html = Html::parse_fragment(&html[..]);
 
     for td in html.select(&selector) {
         let td_title = td.value().attr("title").unwrap_or("").to_owned();
-        let text = td.text().collect::<Vec<_>>();
-        let class =text.get(4).unwrap_or(&"").to_owned().to_owned();
 
-        let krotka = Krotka::new(td_title,class);
+        let text = td.text().collect::<Vec<_>>();
+
+        let class = text.get(4).unwrap_or(&"").to_owned().to_owned();
+
+        let krotka = Krotka::new(td_title, class);
+
         result.push(krotka);
     }
     trasnsponse(result)
 }
 
-async fn extract_groups(sid:&String) -> Vec<String> {
+async fn extract_groups(sid: &String) -> Vec<String> {
     let html = get_plan_site(sid, "").await.expect("ERROR: Couldnt get groups site.");
 
     let mut result: Vec<String> = Vec::new();
+
     let selector = Selector::parse(r#"a[class=aMenu]"#).unwrap();
+
     let html = Html::parse_fragment(&html[..]);
 
     for a in html.select(&selector) {
         let group = a.text().next().unwrap();
+
         result.push(group.to_owned());
     }
     result
 }
 
-fn trasnsponse<T:Clone>(matrix: Vec<T>) -> Vec<T> {
+fn trasnsponse<T: Clone>(matrix: Vec<T>) -> Vec<T> {
     if matrix.len() == 0 {
         return matrix;
     }
     let mut new_matrix: Vec<T> = Vec::new();
+
     let mut i: usize = 0;
+
     let mut offset: usize = 0;
 
     loop {
         new_matrix.push(matrix[i].clone());
+
         i += VMAX;
+
         if i >= VMAX * HMAX {
             offset += 1;
             i = offset;
         }
+
         if new_matrix.len() == VMAX * HMAX {
             break;
         }
@@ -137,7 +164,9 @@ fn trasnsponse<T:Clone>(matrix: Vec<T>) -> Vec<T> {
 
 async fn get_sid(client: &reqwest::Client, url: &str) -> Result<String, reqwest::Error> {
     let body = client.get(url).send().await?.text().await?;
+
     let selector = Selector::parse(r#"form[name="aaa"]"#).unwrap();
+
     let html = Html::parse_fragment(&body[..]);
 
     let result = html
@@ -153,6 +182,7 @@ async fn get_sid(client: &reqwest::Client, url: &str) -> Result<String, reqwest:
     }
 
     let result: Vec<&str> = result.split('=').collect();
+
     let sid = String::from(result[1]);
 
     Ok(sid)
@@ -160,6 +190,7 @@ async fn get_sid(client: &reqwest::Client, url: &str) -> Result<String, reqwest:
 
 fn get_headers() -> reqwest::header::HeaderMap {
     let mut headers2 = reqwest::header::HeaderMap::new();
+
     let headers = [
         (
             "User-Agent",
@@ -202,9 +233,11 @@ async fn login(
         ("view_height", "1080"),
         ("view_width", "1920"),
     ];
+
     let headers = get_headers();
 
     let mut url: String = String::from("https://s1.wcy.wat.edu.pl/ed1/index.php?sid=");
+
     url.push_str(sid);
 
     let post = client
@@ -224,12 +257,16 @@ async fn get_plan_site(sid: &String, group: &str) -> Result<String, reqwest::Err
         "https://s1.wcy.wat.edu.pl/ed1/logged_inc.php?mid=328&iid=20192&pos=0&rdo=1&sid=",
     );
     let mut group_base: String = String::from("&exv=");
+
     url.push_str(&sid[..]);
+
     group_base.push_str(group);
+
     url.push_str(&group_base[..]);
 
     println!("{}", url);
 
     let post = client.post(&url[..]).send().await?.text().await?;
+
     Ok(post)
 }
